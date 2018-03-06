3baba504e931ddcc1ea789a8768d781afdbc47cc
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-06 17:23:38 +0000

    Add basic RestaurantTableViewController

---
+++ FoodPin/RestaurantTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by Jonathan Tang on 06/03/2018.
+//  Copyright © 2018 jtang0506. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+  
+  var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl",
+                         "Petite Oyster", "For Kee Restaurant", "Po's Atelier",
+                         "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso",
+                         "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf",
+                         "Five Leaves", "Cafe Lore", "Confessional", "Barrafina",
+                         "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return restaurantNames.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellIdentifier = "Cell"
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+    
+    cell.textLabel?.text = restaurantNames[indexPath.row]
+    cell.imageView?.image = UIImage(named: "restaurant")
+    
+    return cell
+  }
+  
+}
