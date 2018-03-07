65c24f91cb51c5f54ee739366876703b2226f710
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-07 10:47:40 +0000

    Replace delete option with UIContextualAction
    Now includes both delete and image and name sharing via right swipe

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -109,14 +109,40 @@ class RestaurantTableViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
     // Delete the row from the data source
     if editingStyle == .delete {
-      restaurantNames.remove(at: indexPath.row)
-      restaurantImages.remove(at: indexPath.row)
-      restaurantLocations.remove(at: indexPath.row)
-      restaurantTypes.remove(at: indexPath.row)
-      restaurantIsVisited.remove(at: indexPath.row)
+
     }
     
-    tableView.deleteRows(at: [indexPath], with: .fade)
+    
   }
   
+  override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+      self.restaurantNames.remove(at: indexPath.row)
+      self.restaurantImages.remove(at: indexPath.row)
+      self.restaurantLocations.remove(at: indexPath.row)
+      self.restaurantTypes.remove(at: indexPath.row)
+      self.restaurantIsVisited.remove(at: indexPath.row)
+      
+      self.tableView.deleteRows(at: [indexPath], with: .fade)
+      
+      completionHandler(true)
+    }
+    
+    let shareAction = UIContextualAction(style: .normal, title: "Share") { (action, sourceView, completionHandler) in
+      let defaultText = "Just checking in at " + self.restaurantNames[indexPath.row]
+      let activityController: UIActivityViewController
+      
+      if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row]) {
+        activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+      } else {
+        activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+      }
+
+      self.present(activityController, animated: true, completion: nil)
+      completionHandler(true)
+    }
+    
+    let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
+    return swipeConfiguration
+  }
 }
