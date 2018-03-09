7463d3670317d9b8f148bcf085f724a15e8f1a87
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-09 12:52:02 +0000

    Create header view for restaurant detail view

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -13,10 +13,19 @@ class RestaurantDetailViewController: UIViewController {
   // MARK: - Properties
   var restaurant: Restaurant = Restaurant()
   
+  @IBOutlet var tableView: UITableView!
+  @IBOutlet var headerView: RestaurantDetailHeaderView!
+  
   // MARK: - View controller life cycle
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.largeTitleDisplayMode = .never
+    
+    // Configure header view
+    headerView.nameLabel.text = restaurant.name
+    headerView.typeLabel.text = restaurant.type
+    headerView.headerImageView.image = UIImage(named: restaurant.image)
+    headerView.heartImageView.isHidden = !restaurant.isVisited
   }
   
 }
---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Jonathan Tang on 09/03/2018.
+//  Copyright © 2018 jtang0506. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+  
+  @IBOutlet var headerImageView: UIImageView!
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var typeLabel: UILabel!
+  @IBOutlet var heartImageView: UIImageView!
+  
+}
