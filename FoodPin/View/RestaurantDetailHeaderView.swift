010708e9de0fc637e23021ee4208afdaebc1024d
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-09 15:19:18 +0000

    Tweak header view to match design
    Change image view scaling to aspect fill
    Change name label to take 0 lines to prevent truncation
    Round corners on restaurant type label
    Load heart image as a white image (via templating)
    Dim background image using UIView overlay

--- FoodPin/View/RestaurantDetailHeaderView.swift
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -11,8 +11,22 @@ import UIKit
 class RestaurantDetailHeaderView: UIView {
   
   @IBOutlet var headerImageView: UIImageView!
-  @IBOutlet var nameLabel: UILabel!
-  @IBOutlet var typeLabel: UILabel!
-  @IBOutlet var heartImageView: UIImageView!
+  @IBOutlet var nameLabel: UILabel! {
+    didSet {
+      nameLabel.numberOfLines = 0
+    }
+  }
+  @IBOutlet var typeLabel: UILabel! {
+    didSet {
+      typeLabel.layer.cornerRadius = 5.0
+      typeLabel.layer.masksToBounds = true
+    }
+  }
+  @IBOutlet var heartImageView: UIImageView! {
+    didSet {
+      heartImageView.image = UIImage(named: "heart-tick")?.withRenderingMode(.alwaysTemplate)
+      heartImageView.tintColor = .white
+    }
+  }
   
 }
