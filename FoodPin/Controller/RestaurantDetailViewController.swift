9ef272e54026721c7697094cb8b5ea336939e4c8
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-13 23:40:38 +0000

    Fix issue of description showing object description, not summary

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -83,7 +83,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDelegate, UIT
       return cell
     case 2:
       let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
-      cell.descriptionLabel.text = restaurant.description
+      cell.descriptionLabel.text = restaurant.summary
       cell.selectionStyle = .none
       
       return cell
